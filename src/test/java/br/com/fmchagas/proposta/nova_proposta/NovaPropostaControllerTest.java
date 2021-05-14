189b43afb17f73a13c32da6d550691ae1d43d21c
Fernando Chagas <fermaccha@gmail.com>
2021-05-14 12:27:27 -0400

    testes controlador - remove teste criado pelo spring

--- src/test/java/br/com/fmchagas/proposta/nova_proposta/NovaPropostaControllerTest.java
+++ src/test/java/br/com/fmchagas/proposta/nova_proposta/NovaPropostaControllerTest.java
@@ -3,28 +3,18 @@ package br.com.fmchagas.proposta.nova_proposta;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.math.BigDecimal;
-import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.stubbing.OngoingStubbing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
-import br.com.fmchagas.proposta.cliente_externo.solicitacao.RestrincaoSolicitacao;
-import br.com.fmchagas.proposta.cliente_externo.solicitacao.SolicitacaoCliente;
-import br.com.fmchagas.proposta.cliente_externo.solicitacao.SolicitacaoClienteRequest;
 import br.com.fmchagas.proposta.cliente_externo.solicitacao.SolicitacaoClienteResponse;;
 
 @ExtendWith(SpringExtension.class) // dizer ao JUnit 5 para ativar o suporte ao Spring
@@ -38,7 +28,9 @@ class NovaPropostaControllerTest {
 	@MockBean
 	private PropostaRepository propostaRepository;
 	@MockBean
-	private SolicitacaoCliente solicitacaoCliente;
+	private ElegibilidadeNovaProposta elegibilidade;
+	@MockBean
+	SolicitacaoClienteResponse solicitacaoResponse;
 
 	@Autowired
 	private MockMvc mockMvc;
@@ -52,7 +44,7 @@ class NovaPropostaControllerTest {
 
 
 	@Test
-	void deveRetornar422_QuadoDocumentoPropostaNaoForUnico() {
+	void deveRetornar422_QuadoDocumentoDaPropostaExistir() {
 		when(propostaRepository.existsByDocumento("60178303000120")).thenReturn(true);
 
 		String json = "{\"documento\":\"60178303000120\",\"email\":\"email@gmail.com\",\"nome\":\"Dunha\",\"endereco\":\"R a\",\"salario\":\"25000\"}";
@@ -89,14 +81,10 @@ class NovaPropostaControllerTest {
 	@Test
 	void deveRetornar201_QuadoPropostaForValida(){		
 		Proposta proposta = new Proposta("60178303000120", "email@gmail.com", "Dunha", "Rua a", BigDecimal.TEN);
-		SolicitacaoClienteRequest solicitacaoRequest = new SolicitacaoClienteRequest("60178303000120", "Dunha", 1L);
-		SolicitacaoClienteResponse solicitacaoResponse = new SolicitacaoClienteResponse("60178303000120", "Dunha", RestrincaoSolicitacao.SEM_RESTRICAO, 1);
-		
 		
 		when(propostaRepository.existsByDocumento("60178303000120")).thenReturn(false);
 		
-		when(solicitacaoCliente.consultaViaHttp(solicitacaoRequest))
-		.thenReturn(solicitacaoResponse);
+		doNothing().when(elegibilidade).atribuirPara(proposta);
 		
 		when(propostaRepository.save(proposta)).thenReturn(proposta);
 
@@ -107,9 +95,9 @@ class NovaPropostaControllerTest {
 					.content(json)
 					.contentType("application/json"))
 					.andExpect(status().isCreated());
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
 }
