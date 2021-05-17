ac01e282215410550834fd452ff155896bc036ba
Fernando Chagas <fermaccha@gmail.com>
2021-05-17 12:52:16 -0400

    ajusta repository

--- src/main/java/br/com/fmchagas/proposta/cartao/CartaoRepository.java
+++ src/main/java/br/com/fmchagas/proposta/cartao/CartaoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CartaoRepository extends CrudRepository<Cartao, String>{
+public interface CartaoRepository extends CrudRepository<Cartao, Long>{
 
 }
