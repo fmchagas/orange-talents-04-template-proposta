73f473d2a820ff0608ba224b8a6b9fe3761965d3
Fernando Chagas <fermaccha@gmail.com>
2021-05-21 09:32:15 -0400

    configura security para usar authority de SCOPE_

--- src/main/java/br/com/fmchagas/proposta/compartilhado/security/SecurityConfig.java
+++ src/main/java/br/com/fmchagas/proposta/compartilhado/security/SecurityConfig.java
@@ -1,10 +1,10 @@
 package br.com.fmchagas.proposta.compartilhado.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
@@ -14,14 +14,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and()
-			/*.authorizeRequests()
-				.antMatchers("/api/propostas/*").authenticated()
-				.antMatchers("/api/cartoes/*").authenticated()
-			.and()*/
+			.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/api/propostas/*").hasAuthority("SCOPE_proposta")
+				.antMatchers(HttpMethod.POST, "/api/propostas").hasAuthority("SCOPE_proposta")
+				.antMatchers(HttpMethod.POST, "/api/cartoes/**").hasAuthority("SCOPE_proposta")
+				.anyRequest().authenticated()
+			.and()
 			
-		.authorizeRequests().anyRequest().authenticated().and()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			
-		.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+			.oauth2ResourceServer(rsc -> rsc.jwt());
 	}
 }
